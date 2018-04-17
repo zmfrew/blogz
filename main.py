56ab394b9fa4a881d541e2240fb8e3dc7f2d189f
zfrew <z_frew@yahoo.com>
2018-04-16 19:25:33 -0500

    add signup and login features with user validation

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:MyNewPass@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:MyNewPass@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'Joqiweb092134lkj'
 
 
 class Blog(db.Model):
@@ -13,10 +14,23 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(100000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 def input_error(input):
     if input == "":
@@ -24,9 +38,77 @@ def input_error(input):
     else:
         return False
 
+def error_check(text):
+    if " " in text or 3 > len(text) or len(text) > 40:
+        return True
+    else:
+        return False
+
+def password_check(pass1, pass2):
+    if not pass1 == pass2 or pass2 == "" or len(pass1) < 3:
+        return True
+    else:
+        return False
+
 @app.route('/')
 def reroute():
     return redirect('/blog')
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+
+    if request.method == 'POST':
+        
+        username_error = ""
+        password_error = ""
+        verify_password_error = ""
+        email_error = ""
+
+        username = request.form['username']
+        password = request.form['password']
+        verify_password = request.form['verify-password']
+
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            username_error = "That username has already been taken. Please select a new username!"
+
+        if error_check(username):
+            username_error = "That's not a valid username. Please try again."
+
+        if error_check(password):
+            password_error = "That's not a valid password. Please try again."
+
+        if password_check(password, verify_password):
+            verify_password_error = "Passwords don't match. Please try again."
+# TODO: potentially redo this section with error messages in the template
+        if not existing_user and not error_check(username) and not error_check(password) and not password_check(password, verify_password):
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost')
+    
+        return render_template('signup.html', username_error=username_error, password_error=password_error, 
+        verify_password_error=verify_password_error, username=username, 
+        password=password, verify_password=verify_password)
+
+    return render_template('signup.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            flash("Logged in", 'success')
+            return redirect('/newpost')
+        else:
+            flash("User password incorrect, or user does not exist.", "error")
+
+    return render_template('login.html')        
 
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
@@ -75,6 +157,7 @@ def add_blog():
             #return redirect('/blog')
 
     return render_template('newpost.html', title="Add a Blog Entry", title_error=title_error, body_error=body_error)
+
 
 
 if __name__ == '__main__':
